943dcc9bda095931963dcbca25c8e5575c93175e
raaahuuulll <54004208+raaahuuulll@users.noreply.github.com>
2020-10-08 16:40:03 +0530

    Update w2v.py

--- scripts/w2v.py
+++ scripts/w2v.py
@@ -2,18 +2,18 @@ import gensim
 from nltk.tokenize import sent_tokenize
 import os
 
-training_file = open("data/taylorswift.txt")
-poems1 = open("data/keats.txt")
-poems2 = open("data/frost_poems.txt")
-corpus1 = training_file.read().lower().split("\n")
-corpus1 = [sentence for sentence in corpus1 if(
-    sentence != '' and len(sentence) > 1)]
-corpus2 = poems1.read().lower().split("\n")
-corpus2 = [sentence for sentence in corpus2 if(
-    sentence != '' and len(sentence) > 1)]
-corpus3 = poems2.read().lower().split("\n")
-corpus3 = [sentence for sentence in corpus3 if(
-    sentence != '' and len(sentence) > 1)]
+poems1 = open("data/taylorswift.txt", encoding="utf8")
+poems2 = open("data/keats.txt", encoding="utf8")
+poems3 = open("data/frost_poems.txt", encoding="utf8")
+corpus1 = poems1.read().lower().split("\n")
+corpus1 = [sentence for sentence in corpus1
+           if(sentence != '' and len(sentence) > 1)]
+corpus2 = poems2.read().lower().split("\n")
+corpus2 = [sentence for sentence in corpus2
+           if(sentence != '' and len(sentence) > 1)]
+corpus3 = poems3.read().lower().split("\n")
+corpus3 = [sentence for sentence in corpus3
+           if(sentence != '' and len(sentence) > 1)]
 corpus = corpus1+corpus2+corpus3
 words = [[word for word in sentence.split()] for sentence in corpus]
 
